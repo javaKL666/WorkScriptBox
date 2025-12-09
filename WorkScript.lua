c3b144c892f5b7efe9ae632c5e47cd47d972b4a4
javaKL666 <xb6666661979@outlook.com>
2025-12-09 18:01:01 +0800

    Remove redundant assignment in WorkScript.lua

--- WorkScript.lua
+++ WorkScript.lua
@@ -1,1 +1,1 @@
-getgenv().WorkScript = "工脚本" 	loadstring(game:HttpGet("https://raw.githubusercontent.com/javaKL666/JavaGitHubScriptBox/refs/heads/main/Roblox-Scriptbox-WorkScript.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/javaKL666/JavaGitHubScriptBox/refs/heads/main/Roblox-Scriptbox-WorkScript.lua"))()
